77000f9550523389ee4f9f2176cccd0f32002405
Paulo Gustavo Melo Braz e SIlva <pgmbs@cin.ufpe.br>
2026-03-05 13:33:02 -0300

    Update mux.sv

--- mux.sv
+++ mux.sv
@@ -1,12 +1,17 @@
-module mux
-  (
-    output logic f,
-    input  logic a, b, sel
-  ); 
+module mux4to1_32bits (
+    input  logic [31:0] d0, d1, d2, d3, 
+    input  logic [1:0]  sel,            
+    output logic [31:0] y               
+);
 
-  and g1(f1, a, n_sel),
-      g2(f2, b, sel);
-  or  g3(f, f1, f2);
-  not g4(n_sel, sel);
+    always_comb begin
+        case (sel)
+            2'b00:   y = d0;
+            2'b01:   y = d1;
+            2'b10:   y = d2;
+            2'b11:   y = d3;
+            default: y = 32'b0;
+        endcase
+    end
 
 endmodule
